0a9af5494acd46e25db108fd304cc8263b4019de
stiven45ruiz <stiven45ruiz@gmail.com>
2020-12-08 22:14:30 -0500

    Simuador examen

---
+++ Ejercicios/exmen.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementById("nombre")
+const numeroFficha = document.getElementById("numeroFicha")
+const question2 = document.getElementById("question1").value
+
+console.log(question2)
+
